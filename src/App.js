475eed57af879353348f9750a3f6545d56a04f14
Victor502 <victorahorton@gmail.com>
2020-09-16 23:37:46 -0400

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './stork.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="App-Title">Before the stork maternity</h1>
+       <img src={logo} className="App-logo" alt="logo" />
+        <h2 className="App-Title">
+          Coming Soon
+        </h2>
+        
       </header>
     </div>
   );
